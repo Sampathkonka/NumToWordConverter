eb1f01841bd350318f27bfc917cb148afa12068a
Sampath Konka <sam.srinivasarao@gmail.com>
2019-08-28 19:28:19 +0530

    Added Test cases

--- src/test/java/com/main/converter/NumToWordConverterTest.java
+++ src/test/java/com/main/converter/NumToWordConverterTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import com.numtoword.converter.NumToWordConverter;
+import com.numtoword.exception.NumberToWordException;
 
 public class NumToWordConverterTest {
 
@@ -20,18 +21,33 @@ public class NumToWordConverterTest {
 	@Test
 	public void testConvertInput() throws Exception {
 		assertEquals("One", converter.convertInput(1));
-		//("One", converter.convertInput(1));
 	}
 
 	@Test
-	public void testWithLongValue() throws Exception {
-		assertEquals("One", converter.convertInput(95803405));
-		//("One", converter.convertInput(1));
+	public void testConvertInput2() throws Exception {
+		assertEquals("One Million One Hundred and Eleven Thousand One Hundred and Eleven", converter.convertInput(1111111));
 	}
-
 	
+	@Test
+	public void testConvertInput3() throws Exception {
+		assertEquals("Twenty Two", converter.convertInput(22));
+	}
+	@Test
+	public void testConvertInput4() throws Exception {
+		assertEquals("Sixteen", converter.convertInput(16));
+	}
+	@Test(expected = NumberToWordException.class)
+	public void testConvertInput5() throws Exception {
+		 converter.convertInput(1000000012);
+	}
+	@Test
+	public void testConvertInput6() throws Exception {
+		assertEquals("Minus Nine", converter.convertInput(-9));
+	}
+			
 	@After
 	public void tearDown() {
+		converter = null;
 	}
 
 }

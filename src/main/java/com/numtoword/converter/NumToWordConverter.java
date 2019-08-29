ae91115dddb417f70001b972c04fe0fc2b17cfec
Sampathkonka <35022525+Sampathkonka@users.noreply.github.com>
2019-08-29 08:33:50 +0530

    Update NumToWordConverter.java

--- src/main/java/com/numtoword/converter/NumToWordConverter.java
+++ src/main/java/com/numtoword/converter/NumToWordConverter.java
@@ -10,18 +10,25 @@ public class NumToWordConverter {
 
 	public String convertInput(int num) throws Exception {
 		Utility.validateInputNum(num);
+		if(num == 0) 
+			return "Zero";
+		else
+			return compute(num);
+	}
+
+	private String compute(int num) {
 		if (num < 0) {
-			return "Minus " + convertInput(-num);
+			return "Minus " + compute(-num);
 		} else if (num >= 0 && num < 20) {
 			return digits[num];
 		} else if (num >= 20 && num < 100){
 			return ten_multiples[num/10] + ((num%10 != 0) ? " " : "") + digits[num % 10];
 		} else if(num >= 100 && num < 1000) {
-			return convertInput(num/100) + " Hundred" + ((num%100 != 0) ? " and " : "") + convertInput(num%100);			
+			return compute(num/100) + " Hundred" + ((num%100 != 0) ? " and " : "") + compute(num%100);			
 		} else if(num >= 1000 && num < 1000000) {
-			return convertInput(num/1000) + " Thousand" + ((num%1000 != 0) ? " " : "") + convertInput(num%1000);			
+			return compute(num/1000) + " Thousand" + ((num%1000 != 0) ? " " : "") + compute(num%1000);			
 		} else {
-			return convertInput(num/1000000) + " Million" + ((num%1000000 != 0) ? " " : "") + convertInput(num%1000000);
+			return compute(num/1000000) + " Million" + ((num%1000000 != 0) ? " " : "") + compute(num%1000000);
 		}
 	}
 
